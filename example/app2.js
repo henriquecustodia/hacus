6af3cbaa6b648a04b2aa6468c469e7c1a231232f
Henrique Custódia <martinshenrique8@gmail.com>
2016-06-17 00:25:57 -0300

    the two-way data binding structure has been created

--- example/app2.js
+++ example/app2.js
@@ -1,8 +1,37 @@
+component('@model', {
+    selector: '[\\@model]',
+    ui: function (model, element) {
+        var modelAttr = element.getAttribute('@model');
+        
+        if (model[modelAttr]) {
+            element.value = model[modelAttr];
+        }
+        
+        element.onkeyup = function () {
+            model[modelAttr] = element.value;
+        };
+        
+        model.watch(modelAttr, function (id, old, value) {
+            element.value = value;
+        });
+    }
+});
+
 component('app', {
     selector: 'app',
-    template: '<h1>here</h1>',
+    template: '<input @model="name"><button @click="say()">click</button>',
+    helpers: ['@model'],
     controller: function () {
-        
+        var that = this;
+        this.watch('name', function (id, old, newName) {
+            console.log(newName);
+        });
+
+        this.name = 'henrique';
+
+        this.say = function () {
+            alert('Hello, ' + that.name);
+        };
     }
 });
 
