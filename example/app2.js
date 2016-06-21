b8f99772f1d0f4fbc0fe71589107590e578263ad
Henrique Custódia <henriquecustodia@localhost.localdomain>
2016-06-21 00:24:24 -0300

    data-binding is working and watchers as well

--- example/app2.js
+++ example/app2.js
@@ -2,29 +2,41 @@ component('@model', {
     selector: '[\\@model]',
     ui: function (model, element) {
         var modelAttr = element.getAttribute('@model');
-        
-        if (model[modelAttr]) {
-            element.value = model[modelAttr];
-        }
-        
+
+        element.value = model[modelAttr];
+
         element.onkeyup = function () {
             model[modelAttr] = element.value;
         };
-        
-        model.watch(modelAttr, function (id, old, value) {
+
+       var un = model['@watch'](modelAttr, function (old, value) {
+            console.log('model', value)
             element.value = value;
+            un();
         });
+    }
+});
+
+component('@click', {
+    selector: '[\\@click]',
+    ui: function (model, element) {
+        var click = element.getAttribute('@click');
+        var arr = click.split('(');
+
+        element.onclick = function () {
+            model[arr[0]]();
+        };
     }
 });
 
 component('app', {
     selector: 'app',
     template: '<input @model="name"><button @click="say()">click</button>',
-    helpers: ['@model'],
+    helpers: ['@model', '@click'],
     controller: function () {
         var that = this;
-        this.watch('name', function (id, old, newName) {
-            console.log(newName);
+        this['@watch']('name', function (old, newName) {
+            console.log('controller', newName);
         });
 
         this.name = 'henrique';
